71d88e0f406f272e512c091b1dfd93350ed7da6b
Naveen <adith.naveen@gmail.com>
2021-12-09 15:28:43 +0530

    # please include useful message

---
+++ java-works/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.naveen;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		System.out.println("Hello should be in jenkins");
+		for(int i =0; i<20; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("This is last line");
+	}
+}
